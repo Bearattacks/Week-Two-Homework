d0ac230c271954f9e77b6d3ae77f234f52a4a5b1
Tim Collins <timcollinsdesign@gmail.com>
2014-06-22 17:33:37 -0700

    Login screen completed

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Week Two Homework
+//
+//  Created by Tim Collins on 6/21/14.
+//  Copyright (c) 2014 Tim Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Week Two Homework/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  Week Two Homework
+//
+//  Created by Tim Collins on 6/21/14.
+//  Copyright (c) 2014 Tim Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewController : UIViewController
+
+@end
